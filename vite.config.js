fd0749e33d47b14e06b7331a769495fe41b123d4
muradifs <muradif.smajlovic@gmail.com>
2026-01-13 01:32:09 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-5  |   |  
-6  |  /* NOVI v4 način */
-7  |  @import "tailwindcss";
-   |  ^^^^^^^^^^^^^^^^^^^^^^  
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
